5067179ab1efaee35b7028e424197a4a3af527f9
jimmlucas <136761017+jimmlucas@users.noreply.github.com>
2023-06-16 00:20:20 +0100

    Create samtools.sh

---
+++ samtools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash                                                                                                                                                                                             
+
+FILE="/Users/jimmlucas/Tesis/Data/Mapping/"
+
+for f in "$FILE"/*.bam
+  
+  do 
+      OUT_PUT=$(basename "$f" .bam)
+      
+      samtools sort "$FILE/$OUT_PUT.bam" \
+      -o "$FILE/$OUT_PUT.sorted.bam"
+  
+      samtools index "$FILE/$OUT_PUT.sorted.bam"
+
+  done
