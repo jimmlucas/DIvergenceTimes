90d0b3ceb879de61e0567bdea6b7484e354b3745
jimmlucas <136761017+jimmlucas@users.noreply.github.com>
2023-06-16 01:19:47 +0100

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -3,13 +3,11 @@
 FILE="/Users/jimmlucas/Tesis/Data/Mapping/"
 
 for f in "$FILE"/*.bam
-  
-  do 
+do 
       OUT_PUT=$(basename "$f" .bam)
       
       samtools sort "$FILE/$OUT_PUT.bam" \
       -o "$FILE/$OUT_PUT.sorted.bam"
   
       samtools index "$FILE/$OUT_PUT.sorted.bam"
-
-  done
+done
