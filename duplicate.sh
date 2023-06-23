b00e74299550b0546983c4f6dfa073039e9a4406
jimmlucas <jimmy_12222@hotmail.com>
2023-06-23 13:55:21 +0100

    new change using picard

--- duplicate.sh
+++ duplicate.sh
@@ -2,20 +2,18 @@
 
 IMPUT_DIR="/Users/jimmlucas/Tesis/Data/Mapping"
 OUTPUT="/Users/jimmlucas/Tesis/Data/Annotation"
-GATK="/opt/homebrew/Caskroom/miniforge/base/envs/tesi/share/gatk4-4.4.0.0-0/gatk-package-4.4.0.0-local.jar"
 
 for file in $IMPUT_DIR/*.sorted.bam
 do
     FILE="$file"
     OUT_PUT=$(basename "$f" .sorted.bam)
 
-    java -jar $GATK AddOrReplaceReadGroups \
-    -I $FILE \
-    -O $OUT_PUT.RG.bam \
-    -SORT_ORDER coordinate \
-    -RGID $OUT_PUT \
-    -RGLB 01 \
-    -RGPL illumina \
-    -RGPU 001 \
-    -CREATE_INDEX True
+    picard AddOrReplaceReadGroups \
+    I=$FILE \
+    O="$FILE/$OUT_PUT.RG.bam" \
+    RGID="$OUT_PUT" \
+    RGLB=lib1 \
+    RGPL=ILLUMINA \
+    RGPU=unit1 \
+    RGSM="$OUT_PUT"
 done
