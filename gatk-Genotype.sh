b77e02dd415fec1f0b518a2801d52ca83d2fbe4a
jimmlucas <jimmy_12222@hotmail.com>
2023-10-09 01:21:06 +0200

    update

---
+++ gatk-Genotype.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/combined.g.vcf.gz"
+OUTPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/final.vcf.gz"
+REF_GENOME="/Users/jimmlucas/Tesis/Data/Reference_genome/CBS12357_polished_20170509.fasta"
+
+gatk --java-options "-Xmx96g -Xms96g" GenotypeGVCFs \
+-R $REF_GENOME \
+-V $INPUT \
+-O $OUTPUT
--- gatk-compress.sh
+++ gatk-compress.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-INPUT="/Users/jimmlucas/Tesis/Data/VariantCalling/*.g.vcf.gz"
-OUTPUT="/Users/jimmlucas/Tesis/Data/VariantCalling/combined.g.vcf.gz"
+INPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/*.g.vcf.gz"
+OUTPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/combined.g.vcf.gz"
 REF_GENOME="/Users/jimmlucas/Tesis/Data/Reference_genome/CBS12357_polished_20170509.fasta"
 
 for vcf_file in $INPUT;
 do
     echo "Agregando $vcf_file al archivo combinado"
     gatk CombineGVCFs \
-        -R "$reference_genome" \
+        -R "$REF_GENOME" \
         -V "$vcf_file" \
         -O "$OUTPUT"
 done
