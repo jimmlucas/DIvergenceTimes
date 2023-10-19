05158f92efdf7137e5b6c29da70e26edd0c91ce2
jimmlucas <jimmy_12222@hotmail.com>
2023-10-19 12:03:30 +0200

    update

--- gatk-Genotype.sh
+++ gatk-Genotype.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/combined.g.vcf.gz"
+INPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/*.g.vcf.gz"
 OUTPUT="/Users/jimmlucas/Tesis/Data/VariantCalling2/final.vcf.gz"
 REF_GENOME="/Users/jimmlucas/Tesis/Data/Reference_genome/CBS12357_polished_20170509.fasta"
 
